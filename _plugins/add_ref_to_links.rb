58e2b55a65ac962b813b059869f6eea32dff41ce
pact-wheeler <21691991+pact-wheeler@users.noreply.github.com>
2026-01-30 08:38:40 -0800

    Update add_ref_to_links.rb

--- _plugins/add_ref_to_links.rb
+++ _plugins/add_ref_to_links.rb
@@ -1,8 +1,8 @@
 Jekyll::Hooks.register [:posts, :pages, :documents], :post_render do |doc|
   # Only process HTML output
   if doc.output_ext == ".html"
-    doc.output.gsub!(/<a\s+([^>]*href=["'])(https?:\/\/(?!danielphayward\.com)[^"'?#]+)([^"']*)(["'][^>]*)>/i) do |match|
-      before = $1
+    doc.output.gsub!(/<a\s+href=(["'])(https?:\/\/(?!danielphayward\.com)[^"'?#]+)([^"']*)\1([^>]*)>/i) do |match|
+      quote = $1
       url = $2
       after_url = $3
       rest = $4
@@ -12,7 +12,7 @@ Jekyll::Hooks.register [:posts, :pages, :documents], :post_render do |doc|
       utm_param = "#{separator}utm_source=danielphayward.com"
       
       # Reconstruct the link
-      "<a #{before}#{url}#{after_url}#{utm_param}#{rest}>"
+      "<a href=#{quote}#{url}#{after_url}#{utm_param}#{quote}#{rest}>"
     end
   end
 end
